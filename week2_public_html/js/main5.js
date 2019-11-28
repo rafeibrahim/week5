97b25cf43cd07e612633c97c51d409226628d45c
Rafe Ibrahim <rafe.ibrahim00@gmail.com>
2019-11-28 21:15:10 +0200

    added right url for hosting on server

--- week2_public_html/js/main5.js
+++ week2_public_html/js/main5.js
@@ -1,5 +1,6 @@
 'use strict';
-const url = 'http://localhost:3000'; // change url when uploading to server
+//const url = 'http://localhost:3000'; // change url when uploading to server
+const url = 'http://10.114.32.138/app'
 
 // select existing html elements
 const loginWrapper = document.querySelector('#login-wrapper');
