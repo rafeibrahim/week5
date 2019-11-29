88078753936d538b26fed5c2817dd581f6c6dad0
Rafe Ibrahim <rafe.ibrahim00@gmail.com>
2019-11-29 09:06:01 +0200

    changed redirect url in secure.js to /week5

--- secure/server.js
+++ secure/server.js
@@ -6,7 +6,7 @@ module.exports = (app) => {
         if(req.secure){
             next();
         }else{
-            res.redirect(301, `https://${req.headers.host}/app${req.url}`);
+            res.redirect(301, `https://${req.headers.host}/week5${req.url}`);
         }
     })
 }
